805be4837199bd402757320a2c78f6f0ff420b91
f2lpOliveira <contato@fellipefreire.com.br>
2023-03-08 17:32:59 -0300

    exemplo_basico

---
+++ exemplo_basico/src/com/fellipe/projeto/Aula.java
@@ -0,0 +1,9 @@
+package com.fellipe.projeto;
+
+public class Aula {
+
+	public void estudar() {
+		System.out.println("Iniciando projeto...");
+	}
+
+}
---
+++ exemplo_basico/src/exemplo_basico/App.java
@@ -0,0 +1,14 @@
+package exemplo_basico;
+
+import com.fellipe.projeto.Aula;
+
+public class App {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Hello, World!");
+		Aula a1 = new Aula();
+		a1.estudar();
+
+	}
+
+}
